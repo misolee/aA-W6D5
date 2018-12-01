ada2410f411ca354c7a5d5318c8f298c2383c1d8
misolee <mzxmiso@gmail.com>
2018-11-30 17:03:10 -0800

    Add React Widgets Project for W6D5

---
+++ widgets/src/clock.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class Clock extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      time: new Date()
+    };
+    // this.tick = this.tick.bind(this);
+  }
+  
+  render() {
+    return (
+      <div class="donut">
+        <div class="clock">
+          <h1 id="clock">Clock</h1>
+          <p id="clock">
+            {this.state.time.getHours()}:
+            {this.state.time.getMinutes()}:
+            {this.state.time.getSeconds()}
+          </p>
+          <h1 id="clock">Date</h1>
+          <p id="clock">
+            {this.state.time.toDateString() + " "}
+          </p>
+        </div>
+      </div>
+    );
+  }
+  
+  tick() {
+    this.setState({time: new Date()});
+  }
+  
+  componentDidMount() {
+    // this.interval = setInterval(this.tick, 1000);
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+  
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+}
+
+export default Clock;
---
+++ widgets/src/tabs.jsx
@@ -0,0 +1,34 @@
+import React from  'react';
+
+class Tab extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      index: 0
+    };
+    this.tabs = {
+      title: [],
+      content: []
+    };
+  }
+  
+  render(){
+    return (
+      <ul>
+        // this.tabs.forEach
+      </ul>
+    );
+  }
+  
+  
+  
+}
+
+
+
+
+
+
+
+export default Tab;
